2f3abb7e5cd47285e04307b982af4093a2dfd55a
manureyesi <manureyesi@outlook.es>
2018-08-24 15:03:02 +0200

    Archivos
    Pruebas de guardar objeto en Archivo

--- Scraping/src/SCRAPING/Scraping.java
+++ Scraping/src/SCRAPING/Scraping.java
@@ -7,8 +7,13 @@ package SCRAPING;
 
 import UTILES.Credenciales;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -16,11 +21,11 @@ import java.util.Map;
 //Importar Log
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-import org.jsoup.Connection.Method;
-import org.jsoup.Connection.Response;
 
 //Importar Jsoup
 import org.jsoup.Jsoup;
+import org.jsoup.Connection.Method;
+import org.jsoup.Connection.Response;
 
 /**
  *
@@ -28,12 +33,14 @@ import org.jsoup.Jsoup;
  */
 public class Scraping {
     
+    public static String FICHERO = "CAMBIO_IP";
+    
     private static Logger log = Logger.getLogger(Scraping.class);
     
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         
         // Carga el archivo de configuracion de log4J
         PropertyConfigurator.configure("log4j.properties"); 
@@ -41,6 +48,30 @@ public class Scraping {
         /* Este Servicio solo es valido para Dinahosting */
         
         log.info("Iniciando...............................");
+        UTILES.Credenciales creden = null;
+        
+        log.info("Preparandose para leer Archivo de Objetos");
+        try{
+            
+            File file = new File(FICHERO);
+            
+            //Comprobar si existe el Archivo
+            if(file.exists()){
+                //Leendo archivo de Objetos
+                ObjectInputStream leerObjeto = new ObjectInputStream(new FileInputStream(FICHERO));
+                
+                creden = new UTILES.Credenciales();
+                
+                creden = (UTILES.Credenciales)leerObjeto.readObject();
+                
+            }
+            
+        }
+        catch(IOException | ClassNotFoundException ex){
+            log.error("Error al leer archivo en el fichero");
+        }
+        
+        String ip = "";
         
         try{
             
@@ -50,7 +81,7 @@ public class Scraping {
 
             BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));     
 
-            String ip = in.readLine(); 
+            ip = in.readLine(); 
             
             log.info("IP: "+ip);
             
@@ -62,35 +93,57 @@ public class Scraping {
             log.error("Error al recuperar IP");
         }
         
-        Response response = null;
+        if(ip.compareTo(Credenciales.IP_ACTUAL) != 0){
         
-        try{
+            Response response = null;
             
-            Map<String, String> cokies = new HashMap<String, String>();
-            
-            log.info("Preparando Inicio de Sesion");
-            
-            cokies = Jsoup.connect(Credenciales.URL+"/login").method(Method.POST)
-                    .data("id", Credenciales.USUARIO)
-                    .data("password", Credenciales.CONTRASENA)
-                    .data("recordar", "0")
-                    .execute().cookies();
-            
-            log.info("Cokies: "+cokies.toString());
-            
-            response = Jsoup.connect(Credenciales.URL+"/dominio/procesar-formulario-zonas-modificar-ajax/_producto/"+Credenciales.DOMINIO).method(Method.POST)
-                .data("tipo_zona", Credenciales.TIPO_ZONA)
-                .data("host_ipv4", Credenciales.NOMBRE_HOST)
-                .data("ipv4", Credenciales.IP_NUEVA)
-                .data("destino_original", Credenciales.IP_ACTUAL)
-                .cookies(cokies)
-                .execute();
-            
-            log.info("IP " + Credenciales.IP_NUEVA + " actualiza con exito para el Host " + Credenciales.NOMBRE_HOST);
-            
-        }
-        catch(Exception ex){
-            log.error("Error al modificar la IP");
+            Credenciales.IP_NUEVA = ip;
+
+            try{
+
+                Map<String, String> cokies = new HashMap<String, String>();
+
+                log.info("Preparando Inicio de Sesion");
+
+                cokies = Jsoup.connect(Credenciales.URL+"/login").method(Method.POST)
+                        .data("id", Credenciales.USUARIO)
+                        .data("password", Credenciales.CONTRASENA)
+                        .data("recordar", "0")
+                        .execute().cookies();
+
+                log.info("Cokies: "+cokies.toString());
+
+                response = Jsoup.connect(Credenciales.URL+"/dominio/procesar-formulario-zonas-modificar-ajax/_producto/"+Credenciales.DOMINIO).method(Method.POST)
+                    .data("tipo_zona", Credenciales.TIPO_ZONA)
+                    .data("host_ipv4", Credenciales.NOMBRE_HOST)
+                    .data("ipv4", Credenciales.IP_NUEVA)
+                    .data("destino_original", Credenciales.IP_ACTUAL)
+                    .cookies(cokies)
+                    .execute();
+
+                log.info("IP " + Credenciales.IP_NUEVA + " actualiza con exito para el Host " + Credenciales.NOMBRE_HOST);
+                
+                Credenciales.IP_ACTUAL = Credenciales.IP_NUEVA;
+                
+            }
+            catch(Exception ex){
+                log.error("Error al modificar la IP");
+            }
+
+            try{
+
+                //Guardando en Fichero
+                log.info("Preparando para guarda objeto en Archivo");
+                ObjectOutputStream guardarFichero = new ObjectOutputStream(new FileOutputStream(FICHERO));
+                guardarFichero.writeObject(creden);
+                guardarFichero.close();
+                log.info("Objeto guardado con exito en el archivo " +FICHERO);
+
+            }
+            catch(IOException ex){
+                log.error("Error al guardar Objeto en el fichero");
+            }
+        
         }
         
     }
--- Scraping/src/UTILES/Credenciales.java
+++ Scraping/src/UTILES/Credenciales.java
@@ -11,16 +11,53 @@ package UTILES;
  */
 public class Credenciales {
     
-    public static String USUARIO = "";
-    public static String CONTRASENA = "";
+    public static String USUARIO = "manureyesi";
+    public static String CONTRASENA = "Maindo.11";
     
     public static String URL = "https://panel.dinahosting.com";
     
     public static String DOMINIO = "fiandeira.es";
     
     public static String IP_NUEVA = "";
-    public static String IP_ACTUAL = "";
+    public static String IP_ACTUAL = "10.10.10.2";
     
     public static String NOMBRE_HOST = "prueba";
     public static String TIPO_ZONA = "A";
+    
+    public Credenciales(){}
+
+    public static void setUSUARIO(String USUARIO) {
+        Credenciales.USUARIO = USUARIO;
+    }
+
+    public static void setCONTRASENA(String CONTRASENA) {
+        Credenciales.CONTRASENA = CONTRASENA;
+    }
+
+    public static void setURL(String URL) {
+        Credenciales.URL = URL;
+    }
+
+    public static void setDOMINIO(String DOMINIO) {
+        Credenciales.DOMINIO = DOMINIO;
+    }
+
+    public static void setIP_NUEVA(String IP_NUEVA) {
+        Credenciales.IP_NUEVA = IP_NUEVA;
+    }
+
+    public static void setIP_ACTUAL(String IP_ACTUAL) {
+        Credenciales.IP_ACTUAL = IP_ACTUAL;
+    }
+
+    public static void setNOMBRE_HOST(String NOMBRE_HOST) {
+        Credenciales.NOMBRE_HOST = NOMBRE_HOST;
+    }
+
+    public static void setTIPO_ZONA(String TIPO_ZONA) {
+        Credenciales.TIPO_ZONA = TIPO_ZONA;
+    }
+    
+    
+    
 }
